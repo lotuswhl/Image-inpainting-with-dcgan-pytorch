17f24b55f74ef1e77906f509dd2ad0a7e009f5e4
lotuswhl <2857978398@qq.com>
2018-06-06 21:59:22 +0800

    update for impainting

--- image_impainting_with_naive_dcgan.py
+++ image_impainting_with_naive_dcgan.py
@@ -2,10 +2,9 @@
 """image impainting use naive dcgan"""
 
 import sys
-from boto.auth import sha256
 sys.path.append(".")
 from models.naive_dcgan import Generator, Discriminator, init_weights
-from utils.image_utils import get_image_from_path
+from utils.image_utils import get_tensor_image, save_tensor_images
 from __future__ import print_function
 import os
 import argparse
@@ -63,6 +62,9 @@ parser.add_argument("--netD_path", required=True,
 parser.add_argument("--random_seed", type=int,
                     help="you can specify the random seed if you want")
 
+parser.add_argument("--lamd", type=float,default=0.1,
+                    help="lamd ,coefficients of perceptual loss")
+
 parser.add_argument("--aligned_images", type=str, narg="+",
                     help="input source aligned images for mask and impainting")
 
@@ -102,6 +104,8 @@ num_iters = int(args.num_iters)
 
 z_dim = int(args.z_dim)
 
+lamd = float(args.lamd)
+
 # number of image channels
 num_channels = 3
 
@@ -138,13 +142,12 @@ print(generator)
 print("Discriminator Info:")
 print(discriminator)
 
-# 初始化潜变量z，这个z的目的是匹配待修补的一个batch的图像在generator的底层分布来源
-z = torch.randn(args.batch_size, z_dim, 1, 1).to(device)
+image_shape = [num_channels, args.image_size, args.image_size]
 
-image_shape = [args.image_size, args.image_size, num_channels]
+criteria = nn.BCELoss()
 
 
-def impainting(n_epochs):
+def impainting():
     # 创建输出文件夹
     output_dir = args.output_dir
     source_imagedir = os.path.join(output_dir, "source_images")
@@ -165,99 +168,64 @@ def impainting(n_epochs):
         hidx = min(num_images, (idx + 1) * args.batch_size)
         realBatchSize = hidx - lidx
 
-        batch_images = [get_image_from_path(imgpath) for imgpath in args.aligned_images[lidx:hidx]]
-        batch_images = np.array(batch_images).astype(np.float32)
-        if realBatchSize < args.batch_size:
-            print("number of batch images : ", realBatchSize)
-            # 如果需要修补的图片没有一个batch那么多，用0来填充
-            batch_images = np.pad(batch_images, [(0, args.batch_size - realBatchSize), (0, 0), (0, 0), (0, 0)], "constant")
-            batch_images = batch_images.astype(np.float32)
+        batch_images = [get_tensor_image(imgpath) for imgpath in args.aligned_images[lidx:hidx]]
+        batch_images = batch_images.to(device)
+        # if realBatchSize < args.batch_size:
+        #     print("number of batch images : ", realBatchSize)
+        #     # 如果需要修补的图片没有一个batch那么多，用0来填充
+        #     batch_images = np.pad(batch_images, [(0, args.batch_size - realBatchSize), (0, 0), (0, 0), (0, 0)], "constant")
+        #     batch_images = batch_images.astype(np.float32)
         
         # 输入的原始图片已经准备好，开始准备mask
         # 暂时只提供中心mask
-        mask = np.ones(shape=image_shape)
+        mask = torch.ones(image_shape).to(device)
         imageCenterScale = 0.25
         lm = args.image_size * imageCenterScale
         hm = args.image_size * (1 - imageCenterScale)
         # 将图像中心mask为0
         mask[lm:hm, lm:hm, :] = 0.0
-        masked_batch_images = np.multiply(batch_images, mask)
+        masked_batch_images = torch.mul(batch_images, mask).to(device)
 
-        real_rows = np.ceil(realBatchSize / 8)
-        real_cols = min(8, realBatchSize)
+        # 先保存一下原始图片和masked图片
+        save_tensor_images(batch_images.detach(),os.path.join(source_imagedir,"source_image_batch_{}.png".format(idx)))
+        save_tensor_images(masked_batch_images.detach(), os.path.join(masked_imagedir, "masked_image_batch_{}.png".format(idx)))
 
-    for epoch in range(n_epochs):
-        for i, data in enumerate(dataloader, 0):
-            # first update discriminator's parameters
-            # max(log(D(x))+log(1-D(G(z))))
-            # ------------------------------
-            # first train for real images
+       
 
-            # zero grad
-            discriminator.zero_grad()
-            real_batch_data = data[0].to(device)
-            batch_size = real_batch_data.size(0)
-            batch_labels = torch.full((batch_size,), label_real, device=device)
-            # forward to get output of D for real images
-            real_batch_output = discriminator(real_batch_data)
-            # compute loss for D_real
-            loss_D_real = criteria(real_batch_output, batch_labels)
-            # backward the gradients
-            loss_D_real.backward()
+        z_hat = torch.rand(low=-1.0,high=1.0,[realBatchSize,z_dim],require_grad=True)
+        opt = optim.Adam(z_hat,lr=args.lr)
+        for p in generator.parameters():
+            p.require_grad=False
+        for p in discriminator.parameters():
+            p.require_grad=False
+        
+        print("start impainting iteration for batch : {}".format(idx))
 
-            loss_D_real_mean = loss_D_real.mean().item()
+        for iteration in range(args.num_iters):
+            # 对每一个batch的图像分别迭代impainting
+            z_hat.zero_grad()
+            batch_images_g = generator(z_hat)
+            batch_images_g_masked = torch.mul(batch_images_g,1-mask).to(device)
+            impainting_images = (batch_images_g_masked+masked_batch_images).to(device)
 
-            # now we train with fake images generated by G
-            # first sample from latent variable z
-            fake_batch_z = torch.randn(batch_size, z_dim, 1, 1, device=device)
-            # then generate fake images
-            fake_batch_images = generator(fake_batch_z)
-            # generate fake labels
-            batch_labels.fill_(label_fake)
-            # forward D to get output
-            # !这里使用detach,是为了防止判别网络通过生成网络的输出值拷贝改变其值,进而影响生成网络的梯度计算
-            fake_batch_output = discriminator(fake_batch_images.detach())
-            # compute loss of D for fake images
-            loss_D_fake = criteria(fake_batch_output, batch_labels)
-            # backward
-            loss_D_fake.backward()
+            loss_context = torch.norm(
+                (batch_images_g_masked-torch.mul(batch_images_g,mask).to(device)),p=1)
+            dis_output = discriminator(impainting_images)
+            batch_labels = torch.full((realBatchSize,), 1, device=device)
+            loss_perceptual = criteria(batch_labels,dis_output)
 
-            loss_D_fake_mean = loss_D_fake.mean().item()
+            total_loss = loss_context + lamd*loss_perceptual
+            print("\rbatch {} : iteration : {:4} , context_loss:{:.4f},percptual_loss:{:4f}".format(idx,iteration,loss_context,loss_perceptual),end="")
+            total_loss.back_ward()
+            opt.step()
+            z_hat = torch.clamp(z_hat,low=-1.0,high=1.0).to(device)
 
-            loss_D_total = loss_D_fake + loss_D_real
+            if iteration % 100==0:
+                # 保存impainting 图片结果
+                print("\nsaving impainted images for batch: {} , iteration:{}".format(idx,iteration))
+                save_tensor_images(impainting_images, os.path.join(impainted_imagedir,"impainted_image_batch_{}_iteration_{}.png".format(idx,iteration)))
 
-            # update G parameters
-            optim_D.step()
-
-            # now it's G's turn
-            # ---------------------------------
-            # let's maximize logD(G(z))
-            generator.zero_grad()
-            # mark label real for G
-            batch_labels.fill_(label_real)
-            # 我们需要判别网络更新之后的判别结果,就像银行知道了假币的缺陷,那么制造假币的需要知道银行掌握的信息(判据)
-            batch_output_G = discriminator(fake_batch_images)
-            # compute loss for G
-            loss_G = criteria(batch_output_G, batch_labels)
-            # compute gradient for G
-            loss_G.backward(retain_graph=True)
-
-            loss_G_mean = loss_G.mean().item()
-
-            # update parameters for G
-            optim_G.step()
-
-            print("epoch:{}/{},n_batch:{}/{},loss_real_D:{:.4},loss_fake_D:{:.4},loss_G:{:.4}".format(
-                epoch, n_epochs, i, len(dataloader), loss_D_real_mean, loss_D_fake_mean, loss_G_mean))
-
-            if i % 100 == 0:
-                # sample every 100 batch
-                tvutils.save_image(
-                    real_batch_data, "%s/sample_real.png" % args.output_dir, normalize=True)
-                fake_batch_images = generator(sample_batch_z)
-                tvutils.save_image(fake_batch_images.detach(), "%s/sample_fake_images_epoch%03d_%s.png" % 
-                                   (args.output_dir, epoch, args.dataset), normalize=True)
 
 
 if __name__ == "__main__":
-    train(num_epochs)
+    impainting()
--- utils/image_utils.py
+++ utils/image_utils.py
@@ -1,12 +1,33 @@
 #!-*- coding:utf-8 -*-
-"""image processing utils"""
+"""image processing utils for pytorch"""
 from imageio import imread
-from imageio import imsave
 import numpy as np
+import torchvision.utils as tvls
+import torchvision.transforms as transforms
 
 def get_image_from_path(path):
-    img = imread(path).astype(np.float32)/255.0
+    """
+    从输入路径读取单个图像 并返回PIL Image对象
+    """
+    img = imread(path)
     return img
 
-def save_image_to_path(path,img):
-    imsave(path,(255.0*img).astype(np.uint8))
+def image_to_tensor(image):
+    """
+    将输入的PIL Image对象转为pytorch的Tensor对象,channelximage_heightximage_width格式
+    """
+    trans = transforms.Compose([transforms.ToTensor()])
+    return trans(image)
+
+def get_tensor_image(path):
+    img = get_image_from_path(path)
+    return image_to_tensor(img)
+
+def save_tensor_images(images,filename,nrow=None,normalize=True):
+    """
+    将输入的image tensor数组存为一张图片,默认nrow为8,也就是一行显示8个图片
+    """
+    if not nrow:
+        tvls.save_image(images, filename, normalize=normalize)
+    else:
+        tvls.save_image(images, filename, normalize=normalize,nrow=nrow)
